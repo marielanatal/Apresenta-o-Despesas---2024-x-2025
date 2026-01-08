f87571c4f222ed3863f5f9c81931031881d37b59
marielanatal <mariela.natal@gmail.com>
2026-01-08 07:56:08 -0300

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Dashboard de Despesas",
-    layout="wide"
-)
-
-st.title("🎬 Dashboard de Despesas 2024 x 2025")
-st.markdown("Análise comparativa de gastos com visão executiva e detalhamento estratégico.")
